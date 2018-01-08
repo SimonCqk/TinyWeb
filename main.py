5b39f3aa962eb4b19ba6928be29ad312003a6ee3
Cqk <cqk0100@gmail.com>
2018-01-08 18:28:20 +0800

    use thread pool to execute tasks instead of pure threads.

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ main executor of TINY WEB.
 '''
 import sys
 import socket
-import threading
+from concurrent.futures import ThreadPoolExecutor
 from handler import handler
 
 HOST = '127.0.0.1'
 PORT = 80
 ADDRESS = (HOST, PORT)
-MAX_INCOME = 10
+MAX_INCOME = 10  # max income connection.
 
 
 def main(argv):
@@ -18,13 +18,14 @@ def main(argv):
         return
     tcp_ser_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_ser_sock.bind(ADDRESS)
-    tcp_ser_sock.listen(MAX_INCOME)  # max income connection is 5.
+    tcp_ser_sock.listen(MAX_INCOME)
+    pool = ThreadPoolExecutor(max_workers=MAX_INCOME)
     while True:
         tcp_cli_sock, address = tcp_ser_sock.accept()
         print('Accept connection from ({host},{port})'.format(host=address[0], port=address[1]))
-        t = threading.Thread(target=handler, args=(tcp_cli_sock,))
-        t.start()
+        pool.submit(handler, tcp_cli_sock)
     tcp_ser_sock.close()  # may never execute
+    pool.shutdown()
 
 
 if __name__ == '__main__':
